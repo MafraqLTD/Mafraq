f9411b72b83e237523f12eb127591b717fd6edb2
Ahmed Mones <agprosup@gmail.com>
2024-04-11 21:56:28 +0300

    do changed request

--- presentation/src/main/java/com/mafraq/presentation/design/components/ColumnPreview.kt
+++ presentation/src/main/java/com/mafraq/presentation/design/components/ColumnPreview.kt
@@ -3,7 +3,9 @@ package com.mafraq.presentation.design.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,12 +14,14 @@ import com.mafraq.presentation.design.theme.MafraqTheme
 
 @Composable
 fun ColumnPreview(
+    contentPadding: PaddingValues = PaddingValues(),
     horizontalAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
     verticalArrangement: Arrangement.Vertical = Arrangement.spacedBy(MafraqTheme.sizes.small),
     content: @Composable ColumnScope.() -> Unit,
 ) = MafraqTheme {
     Column(
         modifier = Modifier
+            .padding(contentPadding)
             .fillMaxWidth(),
         horizontalAlignment = horizontalAlignment,
         verticalArrangement = verticalArrangement,
--- presentation/src/main/java/com/mafraq/presentation/features/home/components/ItemResultSearch.kt
+++ presentation/src/main/java/com/mafraq/presentation/features/home/components/ItemResultSearch.kt
@@ -2,11 +2,14 @@ package com.mafraq.presentation.features.home.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,35 +18,52 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mafraq.presentation.R
+import com.mafraq.presentation.design.components.ColumnPreview
 import com.mafraq.presentation.design.components.Spacer
 import com.mafraq.presentation.design.theme.MafraqTheme
 import com.mafraq.presentation.utils.extensions.painter
 
+
 @Composable
-fun InfoCard(
+fun SearchResultItem(
     headerText: String,
     bodyText: String,
 ) {
-    Box(Modifier.fillMaxWidth()) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = MafraqTheme.shapes.large
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
 
-        Card {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+            Box(contentAlignment = Alignment.Center) {
+
+                Surface(
+                    shape = RoundedCornerShape(
+                        topEnd = MafraqTheme.sizes.large,
+                        bottomEnd = MafraqTheme.sizes.large,
+                    ),
+                    color = MafraqTheme.colors.background.copy(alpha = 0.7f),
+                    modifier = Modifier.size(48.dp)
+                ) {}
+
                 Icon(
-                    painter = R.drawable.ic_location_background.painter,
+                    painter = R.drawable.ic_location_filled.painter,
                     contentDescription = null,
                     tint = Color.Unspecified,
-                    modifier = Modifier.padding(end = 8.dp)
                 )
 
-                Column(modifier = Modifier.fillMaxWidth()) {
-                    Text(text = headerText, style = MafraqTheme.typography.body)
-                    Spacer.ExtraSmall()
-                    Text(
-                        text = bodyText,
-                        style = MafraqTheme.typography.label,
-                        color = Color.Gray
-                    )
-                }
+            }
+
+            Spacer.Small(vertical = false)
+
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(text = headerText, style = MafraqTheme.typography.body)
+                Spacer.ExtraSmall()
+                Text(
+                    text = bodyText,
+                    style = MafraqTheme.typography.label,
+                    color = Color.Gray
+                )
             }
         }
     }
@@ -52,8 +72,10 @@ fun InfoCard(
 
 @Composable
 @Preview(showBackground = true)
-fun SimpleComposablePreview() = ColumnPreview{
-    InfoCard("University of Baghdad", "Iraq,street 2018/9")
-    InfoCard("University of Baghdad", "Iraq,street 2018/9")
-    InfoCard("University of Baghdad", "Iraq,street 2018/9")
+private fun Preview() = ColumnPreview(
+    contentPadding = PaddingValues(MafraqTheme.sizes.medium)
+){
+    SearchResultItem("University of Baghdad", "Iraq,street 2018/9")
+    SearchResultItem("University of Baghdad", "Iraq,street 2018/9")
+    SearchResultItem("University of Baghdad", "Iraq,street 2018/9")
 }
