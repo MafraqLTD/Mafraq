e33baf88d206b2f46f06ab7d7c4e09f09eaa5d69
Ahmed Mones <agprosup@gmail.com>
2024-04-20 16:52:57 +0300

    implement profile screen

--- presentation/src/main/java/com/mafraq/presentation/features/profile/ProfileViewModel.kt
+++ presentation/src/main/java/com/mafraq/presentation/features/profile/ProfileViewModel.kt
@@ -1,6 +1,10 @@
 package com.mafraq.presentation.features.profile
 
 
+import androidx.lifecycle.SavedStateHandle
+import com.mafraq.data.entities.profile.DayOff
+import com.mafraq.data.entities.profile.Gender
+import com.mafraq.data.repository.crm.CRMRepository
 import com.mafraq.presentation.features.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -8,7 +12,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-
+    savedStateHandle: SavedStateHandle,
+    private val crmRepository: CRMRepository,
 ) : BaseViewModel<ProfileUiState, ProfileEvent>(ProfileUiState()), ProfileInteractionListener {
     override fun onLogout() {
         emitNewEvent(ProfileEvent.OnLogout)
@@ -20,9 +25,11 @@ class ProfileViewModel @Inject constructor(
 
     override fun setEmail(value: String) = updateState { copy(email = value, error = null) }
 
-    override fun setAddress(value: String) = updateState { copy(address = value, error = null) }
+    override fun setHomeAddress(value: String) =
+        updateState { copy(homeAddress = value, error = null) }
 
-    override fun setUsername(value: String) = updateState { copy(username = value, error = null) }
+    override fun setWorkAddress(value: String) =
+        updateState { copy(workAddress = value, error = null) }
 
     override fun setPhone(value: String) = updateState { copy(phone = value, error = null) }
 
@@ -31,10 +38,28 @@ class ProfileViewModel @Inject constructor(
     override fun validateFields(): Boolean = state.value.run {
         listOf(
             email,
-            username,
-            address,
+            workAddress,
+            homeAddress,
             phone,
             fullname
         ).all(String::isNotEmpty)
     }
+
+    override fun setGender(value: Gender) = updateState {
+        copy(gender = value)
+    }
+
+    override fun setOffDays(selected: Boolean, offDay: DayOff) = updateState {
+        val offDays = offDays.toMutableSet()
+        if (selected)
+            offDays.add(offDay)
+        else
+            offDays.remove(offDay)
+        copy(offDays = offDays)
+    }
+
+    override fun setBirthday(value: String) = updateState {
+        copy(birthday = value)
+    }
 }
+
