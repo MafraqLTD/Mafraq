2dbf19fb138e17bfda4f3e700bb01b1bd6be666b
Ahmed Mones <agprosup@gmail.com>
2024-04-30 22:07:26 +0300

    fix crash

--- data/src/main/java/com/mafraq/data/remote/dataSource/subscription/employee/EmployeeSubscriptionDataSourceImpl.kt
+++ data/src/main/java/com/mafraq/data/remote/dataSource/subscription/employee/EmployeeSubscriptionDataSourceImpl.kt
@@ -1,9 +1,9 @@
 package com.mafraq.data.remote.dataSource.subscription.employee
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.mafraq.data.entities.Subscriber
 import com.mafraq.data.entities.map.Driver
 import com.mafraq.data.local.driver.DriverLocalDataSource
-import com.mafraq.data.local.driver.DriverLocalDataSourceImpl
 import com.mafraq.data.local.profile.ProfileLocalDataSource
 import com.mafraq.data.remote.dataSource.chat.delete
 import com.mafraq.data.remote.dataSource.chat.insert
@@ -15,30 +15,33 @@ import javax.inject.Inject
 
 class EmployeeSubscriptionDataSourceImpl @Inject constructor(
     firestore: FirebaseFirestore,
-    profileLocalDataSource: ProfileLocalDataSource,
-    subscriberFromEmployeeMapper: SubscriberFromEmployeeMapper,
-    private val driverLocalDataSource: DriverLocalDataSource
+    private val driverLocalDataSource: DriverLocalDataSource,
+    private val profileLocalDataSource: ProfileLocalDataSource,
+    private val subscriberFromEmployeeMapper: SubscriberFromEmployeeMapper,
 ) : EmployeeSubscriptionDataSource {
     override var driver: Driver? = driverLocalDataSource.get()
-    private val employee = requireNotNull(profileLocalDataSource.get())
-    private val subscriber = subscriberFromEmployeeMapper.map(employee)
-    private val root by lazy {
-        firestore.collection(DRIVERS_COLLECTION)
-    }
+    private val root by lazy { firestore.collection(DRIVERS_COLLECTION) }
 
     override suspend fun request(driver: Driver) {
-        driverLocalDataSource.save(driver)
         this.driver = driver
+        val subscriber = getSubscriber()
+        driverLocalDataSource.save(driver)
         root.document(driver.id)
             .collection(PENDING_COLLECTION)
             .insert(id = subscriber.id, entry = subscriber)
     }
 
     override suspend fun cancel() {
+        val subscriber = getSubscriber()
         root.document(requireNotNull(driver?.id))
             .collection(PENDING_COLLECTION)
             .delete(id = subscriber.id)
         this.driver = null
         driverLocalDataSource.delete()
+    }
+
+    private fun getSubscriber(): Subscriber {
+        val employee = requireNotNull(profileLocalDataSource.get())
+        return subscriberFromEmployeeMapper.map(employee)
     }
 }
