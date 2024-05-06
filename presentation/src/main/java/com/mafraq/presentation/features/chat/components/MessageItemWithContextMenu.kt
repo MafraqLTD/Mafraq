eea8c4f0568769c69b4cc239ec542bd3c010ef88
Ahmed Mones <agprosup@gmail.com>
2024-05-06 21:16:47 +0300

    improve chat and implement copy message

--- presentation/src/main/java/com/mafraq/presentation/features/chat/components/MessageItemWithContextMenu.kt
+++ presentation/src/main/java/com/mafraq/presentation/features/chat/components/MessageItemWithContextMenu.kt
@@ -1,6 +1,7 @@
 package com.mafraq.presentation.features.chat.components
 
 import androidx.compose.runtime.Composable
+import com.google.android.play.integrity.internal.o
 import com.mafraq.data.entities.chat.Message
 import com.mafraq.presentation.features.chat.group.GroupChatContextMenuAction
 
@@ -10,14 +11,21 @@ fun MessageItemWithContextMenu(
     message: Message,
     onEditMessage: () -> Unit,
     onDeleteMessage: () -> Unit,
+    onCopyMessage: () -> Unit,
 ) {
     ContextMenu(
         alignment = ContextMenuAlignment.End,
-        actions = GroupChatContextMenuAction.entries.toTypedArray(),
+        actions = GroupChatContextMenuAction.getOptions {
+            if (message.isFromMe)
+                true
+            else
+                it != GroupChatContextMenuAction.Delete
+        },
         onActionClicked = { action ->
             when (action) {
                 GroupChatContextMenuAction.Edit -> onEditMessage()
                 GroupChatContextMenuAction.Delete -> onDeleteMessage()
+                GroupChatContextMenuAction.Copy -> onCopyMessage()
             }
         },
     ) { onClick ->
